c64758c19ea9196ed7e28482084f06c5f7d6c711
axelcrovetto <axelcrovetto2102@gmail.com>
2022-09-18 21:48:10 +0200

    implemented gps.

---
+++ src/app/activity-list/activity-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { IActivity } from '../shared/activity.model';
+import { ActivityService } from '../services/activity.service';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-activity-list',
+  templateUrl: './activity-list.component.html',
+  styleUrls: ['./activity-list.component.css']
+})
+export class ActivityListComponent implements OnInit {
+
+  activities!: IActivity[];
+  totalActivities!: number;
+  totalDistance!: number;
+  firstDate!: Date;
+
+  constructor(private _activityService: ActivityService,
+    private router:RouterModule) { }
+
+  ngOnInit() {
+    this.activities = this._activityService.getActivities();
+    this.totalActivities = this._activityService.getTotalActivities(this.activities);
+    this.totalDistance = this._activityService.getTotalDistance(this.activities);
+    this.firstDate = this._activityService.getFirstDate(this.activities);
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { MapComponent } from './map/map.component';
+
+import { ActivityService } from './services/activity.service';
+import { HttpClientModule } from '@angular/common/http';
+import { appRoutes } from '../routes';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivityListComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ActivityService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
